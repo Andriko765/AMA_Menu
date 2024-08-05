9415f89a786a48d82882480cf075baed4d4738c2
Andriko765 <andrik096108@gmail.com>
2024-08-05 13:45:38 +0300

    fix_errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { Nav,Footer} from "./components"
-import { Cider, Pizza,Snacks,NoAlchohol, Hotdog, Shots } from "./sections"
-import Alchohol from "./sections/Alchohol"
-
+import { Nav, Footer } from './components';
+import { DataCollection } from './api/Data';
 const App = () => {
-  return (
-    <main className=" relative bg-[#1d1b20] h-full">
-          <Nav />
-        <Cider/>
-        <Shots/>
-        <Alchohol/>
-        <Pizza/>
-        <Hotdog/>
-        <Snacks/>
-        <NoAlchohol/>
-        <Footer/>
+	return (
+		<main className=" relative bg-[#1d1b20] h-full">
+			<Nav />
+			<DataCollection></DataCollection>
+			<Footer />
+		</main>
+	);
+};
 
-    </main>
-  )
-}
-
-export default App
+export default App;
--- src/api/Data.jsx
+++ src/api/Data.jsx
@@ -1,28 +1,23 @@
-import React, {  useEffect , useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+export const ProductContext = React.createContext();
 
-  export const ProductContext = React.createContext(); 
+export const DataCollection = ({ children }) => {
+	const [products, setProducts] = useState([]);
 
- export const DataCollection = ({children,request}) => {
+	useEffect(() => {
+		axios
+			.get('http://localhost:5000/api/products')
+			.then((res) => {
+				setProducts(res.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-
-        axios.get(`https://amaapi.up.railway.app/${request}`)
-        .then( res => {
-       setProducts(res.data)})
-        .catch(err=> console.log(err));
-    }, []);
-
-    return(
-        <ProductContext.Provider value={{ products }}>
-            {children}
-        </ProductContext.Provider>
-        
-    )
-
-}
-
-
+	return (
+		<ProductContext.Provider value={{ products }}>
+			{children}
+		</ProductContext.Provider>
+	);
+};
